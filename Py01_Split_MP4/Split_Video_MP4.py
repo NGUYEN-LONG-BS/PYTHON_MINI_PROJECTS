c05e56706b1818d6c90059032dea44f7a231ea75
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2024-04-27 10:28:17 +0700

    đo thời lượng của video

--- Py01_Split_MP4/Split_Video_MP4.py
+++ Py01_Split_MP4/Split_Video_MP4.py
@@ -1,11 +1,12 @@
 # Note
 # Source: https://stackoverflow.com/questions/67334379/cut-mp4-in-pieces-python
+# Source: https://stackoverflow.com/questions/3844430/how-to-get-the-duration-of-a-video-in-python
 # install library moviepy: pip3 install moviepy
 
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 # Replace the filename below.
-required_video_file = "200921 B03.mp4"
+required_video_file = "01 - QUANT - Buổi 1 - 18.08.mp4"
 
 with open("times.txt") as f:
   times = f.readlines()
@@ -16,3 +17,14 @@ for time in times:
   starttime = int(time.split("-")[0])
   endtime = int(time.split("-")[1])
   ffmpeg_extract_subclip(required_video_file, starttime, endtime, targetname=str(times.index(time)+1)+".mp4")
+
+# Đo thời lượng của video
+# filename = required_video_file
+# def with_moviepy(filename):
+#     from moviepy.editor import VideoFileClip
+#     clip = VideoFileClip(filename)
+#     duration       = clip.duration
+#     fps            = clip.fps
+#     width, height  = clip.size
+#     return duration, fps, (width, height)
+  
