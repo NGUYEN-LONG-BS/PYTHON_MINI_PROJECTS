22f78acd2fcb4212b06c396fc4347f870113136c
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2024-05-04 14:04:58 +0700

    commit

--- Py08_CRUD_With_Tkinter_Mysql_QLSV/Quan_Ly_Nhan_Su.py
+++ Py08_CRUD_With_Tkinter_Mysql_QLSV/Quan_Ly_Nhan_Su.py
@@ -3,9 +3,10 @@
 
 from tkinter import *
 from database import *
+from define import * 
 
 def add():
-    line=id.get() + ' - ' + name.get() +' - '+year.get()
+    line=id.get() + '-' + name.get() + '-' + year.get()
     save(line)
     show()
 
@@ -16,39 +17,48 @@ def show():
         listbox.insert(END,i)
 
 def sort():
-    sv=read
+    sv=read()
     for i in range(len(sv)):
         for j in range(len(sv)): 
-            x, y=sv[1], sv[1]
+            x,y=sv[i],sv[j]
             if x[2]>y[2]:
                 sv[i], sv[j]=y,x
     listbox.delete(0, END)
     for i in sv:
-        listbox.insert(END, 1)
+        listbox.insert(END, i)
 
 root=Tk() 
 #Var
 id=StringVar()
 name=StringVar()
 year=StringVar()
+
+
 root.title('Quản Lý Nhân Sự ')
 root.minsize(height=580, width=500)
-Label(root, text='ÚNG DỤNG QUẢN LÝ NHÂN SỰ',fg='red', font=('cambria',16),width=25).grid(row=0) 
-listbox=Listbox(root, width=80,height=20)
-listbox.grid(row=1, columnspan=2)
-show()
-Label(root, text='Mã NV:').grid(row=2, column=0)
-Entry(root, width=30, textvariable=id).grid(row=2, column=1)
-Label(root, text='Tên Nhân viên:').grid(row=3, column=0)
-Entry(root, width=30, textvariable=name).grid(row=3, column=1) 
-Label(root, text='Năm sinh: ').grid(row=4, column=0)
-Entry(root, width=30, textvariable=year).grid(row=4, column=1)
+Label(root, text='QUẢN LÝ NHÂN SỰ',fg=COLOR_BLACK, font=('cambria',16),width=25).grid(row=0) 
+
+# Icon
+root.iconbitmap(os.path.join(PATH_IMAGES, 'icon.ico'))
+
+Label(root, text='Mã NV:').grid(row=1, column=0)
+Entry(root, width=30, textvariable=id).grid(row=1, column=1)
+Label(root, text='Tên Nhân viên:').grid(row=2, column=0)
+Entry(root, width=30, textvariable=name).grid(row=2, column=1) 
+Label(root, text='Năm sinh: ').grid(row=3, column=0)
+Entry(root, width=30, textvariable=year).grid(row=3, column=1)
 button=Frame(root)
 Button(button, text='Thêm', command=add).pack(side=LEFT) 
-Button(button, text='Xem').pack(side=LEFT)
+Button(button, text='Xem', command=show).pack(side=LEFT)
 Button(button, text='Sắp xếp', command=sort).pack(side=LEFT) 
 Button(button, text='Thoát ', command=root.quit).pack (side=LEFT) 
-button.grid(row=5, column=1)
+button.grid(row=4, column=1)
+
+# Listbox
+listbox=Listbox(root, width=80,height=20)
+listbox.grid(row=5, columnspan=2)
+show()
+
 
 root.mainloop()
 
--- Py08_CRUD_With_Tkinter_Mysql_QLSV/database.py
+++ Py08_CRUD_With_Tkinter_Mysql_QLSV/database.py
@@ -1,10 +1,12 @@
+from define import * 
+
 # path='E:\QLSV.txt'
 # path='C:\Users\ADMIN\Desktop\GITHUB\PYTHON_MINI_PROJECTS\Py08_CRUD_With_Tkinter_Mysql_QLSV\QLSV.txt'
-path='QLSV.txt'
+# path='QLSV.txt'
 
 def save (line):
     try:
-        f=open(path, 'a', encoding='utf8')
+        f=open(PATH_QLSV_TXT, 'a', encoding='utf8')
         f.writelines (line)
         f.writelines ('\n')
         f.close()
@@ -12,9 +14,9 @@ def save (line):
         pass
 
 def read():
-    SV=[]
+    sv=[]
     try:
-        f = open(path, 'r', encoding='utf8')
+        f = open(PATH_QLSV_TXT, 'r', encoding='utf8')
         for i in f:
             data=i.strip()
             arr=data.split('-')
@@ -22,4 +24,4 @@ def read():
         f.close()
     except:
         pass
-    return SV
+    return sv
