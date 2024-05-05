002240e3c1362e6a03ab631bf0c6cf4db082ea44
Quan Nguyen <quann169@gmail.com>
2024-05-05 20:37:19 +0700

    commit

--- Py08_CRUD_With_Tkinter_Mysql_QLSV/My_SQL_Connector.py
+++ Py08_CRUD_With_Tkinter_Mysql_QLSV/My_SQL_Connector.py
@@ -1,5 +1,24 @@
+# pip install mysql
 # pip install mysql-connector-python
+# python -m pip install mysql-connector-python
+# py -3.12 -m pip install mysql-connector-python
+# pip3 install mysql-connector-python-rf
 # https://dev.mysql.com/downloads/installer/
+
+
+import mysql.connector
+
+print('hehehehehehehhehehehehe')
+
 user_name_my_sql = 'root'
 pass_my_sql = 'Ta#9999'
 host_my_sql = 'localhost'   # 3306
+
+db = mysql.connector.connect(user='root',password='Ta#9999',host='localhost')
+
+#query
+code='CREATE DATABASE `test` ;' # 'CREATE SCHEMA `test` ;'
+
+#RUN
+mycursor=db.cursor()
+mycursor.execute(code)
