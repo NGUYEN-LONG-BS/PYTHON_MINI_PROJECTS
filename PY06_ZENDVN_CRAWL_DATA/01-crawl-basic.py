33cd62e3a0e5e5a0511bcf1745d3a8a5daa0136f
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2024-10-07 17:04:37 +0700

    commit

--- PY06_ZENDVN_CRAWL_DATA/01-crawl-basic.py
+++ PY06_ZENDVN_CRAWL_DATA/01-crawl-basic.py
@@ -1,2 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = 'http://demo-php-bookstore.zendvn.com/index.html'
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+print(soup)
+category_one = soup.select_one('#main-menu > li:nth-child(2) > ul > li:nth-child(1) > a')
+
+print(category_one.text + "-----" + category_one.get('href'))
--- PY12_AUTOMATE_FB/automte_FB.py
+++ PY12_AUTOMATE_FB/automte_FB.py
@@ -1,1 +1,6 @@
 from selenium import webdriver
+
+def launchBrower():
+    browser = webdriver.Chrome(service=service(chromẻiverManager().install()))
+    browser.get("https://www.facebook.com/")
+    return browser
