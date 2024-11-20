b804b35f6f9f0ed37a8c41b680411f5331342d7f
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2024-11-20 16:17:05 +0700

    commit

--- PY15_MINI_PROJECTS/miniApp_05_QuanLyGoiThau/CTK/app.py
+++ PY15_MINI_PROJECTS/miniApp_05_QuanLyGoiThau/CTK/app.py
@@ -10,55 +10,45 @@ from datetime import datetime
 # Cấu trúc tên của folder: 24_GOI_THAU_0001_15234685
 # ======================================================================================
 
-
 # ======================================================================================
 # Change the current working directory to the script's directory
 # ======================================================================================
-# Get the directory of the current script
-script_dir = os.path.dirname(os.path.abspath(__file__))
-# Change the current working directory to the script's directory
+script_dir = os.path.dirname(os.path.abspath(__file__)) # Get the directory of the current script
 os.chdir(script_dir)
-
 
 
 # ======================================================================================
 # Tạo tên thư mục
 # ======================================================================================
-
-# Khai báo đường dẫn đến các gói thầu
-PathQuanLyThau = r"\\172.16.0.191\2.0 ksnb\TUAN_AN_GROUP\BAN_KINH_DOANH\QUAN_LY_THAU"
-arr_DanhSachGoiThau = os.listdir(PathQuanLyThau)
-
-print(arr_DanhSachGoiThau)
-
-# Tạo mới folder theo tên có sẵn
-
-# Tạo mới folder có tên "hhhhhhhh"
-folder_name_01_year = "24"
-folder_name_02 = "GOI_THAU"
-folder_name_03_number = str(len(arr_DanhSachGoiThau) + 1).zfill(4)      # hiển thị 4 ký tự
-folder_name_04 = "SoThongBaoMoiThau"
-folder_name_completed = folder_name_01_year + "_" + folder_name_02 + "_" + folder_name_03_number + "_" + folder_name_04
-
-new_folder_path = os.path.join(PathQuanLyThau, folder_name_completed)
-
-# Kiểm tra nếu folder chưa tồn tại, thì tạo mới
-def check_variable_in_array(array, variable):
-    if variable in array:
-        return True
+def Create_New_Folder():
+    # Khai báo đường dẫn đến các gói thầu
+    PathQuanLyThau = r"\\172.16.0.191\2.0 ksnb\TUAN_AN_GROUP\BAN_KINH_DOANH\QUAN_LY_THAU"
+    arr_DanhSachGoiThau = os.listdir(PathQuanLyThau)
+    # print(arr_DanhSachGoiThau)
+    
+    # Tạo tên folder mới
+    folder_name_01_year = COMBOBOX_NamGoiThau.get()[-2:]
+    folder_name_02 = "GOI_THAU"
+    folder_name_03_number = str(len(arr_DanhSachGoiThau) + 1).zfill(4)      # hiển thị 4 ký tự
+    # folder_name_04 = "SoThongBaoMoiThau"
+    folder_name_04 = ENTRY_01_SoThongBaoMoiThau.get().zfill(4)
+    folder_name_completed = folder_name_01_year + "_" + folder_name_02 + "_" + folder_name_03_number + "_" + folder_name_04
+    new_folder_path = os.path.join(PathQuanLyThau, folder_name_completed)
+    
+    # Kiểm tra nếu folder chưa tồn tại, thì tạo mới
+    def check_variable_in_array(array, variable):
+        if variable in array:
+            return True
+        else:
+            return False
+    result = check_variable_in_array(arr_DanhSachGoiThau, folder_name_completed)
+    # Thực hiện tạo folder
+    if result:
+        print(f"The string '{folder_name_completed}' exists in the array.")
     else:
-        return False
-
-result = check_variable_in_array(arr_DanhSachGoiThau, folder_name_completed)
-
-if result:
-    print(f"The string '{folder_name_completed}' exists in the array.")
-else:
-    print(f"The string '{folder_name_completed}' does not exist in the array.")
-    os.makedirs(new_folder_path)
-    print(f"Đã tạo folder: {new_folder_path}")
-
-
+        print(f"The string '{folder_name_completed}' does not exist in the array.")
+        os.makedirs(new_folder_path)
+        print(f"Đã tạo folder: {new_folder_path}")
 
 
 # ======================================================================================
@@ -66,71 +56,241 @@ else:
 # ======================================================================================
 app = CTk()
 app.geometry("900x800")
+# Create the main application window
+app.title("QUẢN LÝ GÓI THẦU")
 
+# Mode Dark or Light
 set_appearance_mode("dark")
 # set_appearance_mode("light")
+
+# Change the color style
+set_default_color_theme(r"json\MoonlitSky.json")
+# set_default_color_theme(r"json\NeonBanana.json") 
+
 
 # ======================================================================================
 # BUTTON
 # ======================================================================================
 def click_handler():
-    print("Button Clicked")
+    # print("Button Clicked")
+    Create_New_Folder()
+    reset_scrollable_frame()
+    set_scrollable_frame()
 
 img = Image.open(r"img\icons8-chat-message-50.png")
 
-btn = CTkButton(master=app, 
+btn_01_TaoThuMuc = CTkButton(master=app, 
                 text="Tạo thư mục mới", 
                 corner_radius=32, 
-                fg_color="#4158D0",
-                hover_color="#C850C0", 
-                border_color="#FFCC70",
+                # fg_color="#4158D0",
+                # hover_color="#C850C0", 
+                # border_color="#FFCC70",
                 border_width=2, 
                 image=CTkImage(dark_image=img, light_image=img),
                 command=click_handler)
-btn.place(x=50, y=700)
+btn_01_TaoThuMuc.place(x=50, y=700)
 
 # ======================================================================================
-# COMBOBOX
+# COMBOBOX_NamGoiThau
 # ======================================================================================
 def change_handler(value):
-    print(f"Selected Value {value}")
+    last_02_chars = value[-2:]
+    print(f"Selected Value {value}, Last Two Characters: {last_02_chars}")
     
 # Get the current year
-current_year = str(datetime.now().year)
+current_year = datetime.now().year
+# Create the array with the specified years
+year_array = [str(current_year - 2), str(current_year - 1), str(current_year), str(current_year + 1)]
 
-combobox = CTkComboBox(master=app, 
-                       values=["2022","2023","2024","2025","2026","2027","2028"], 
-                       fg_color="#0093E9",
-                       border_color="#FBAB7E", 
-                       dropdown_fg_color="#0093E9",
+COMBOBOX_NamGoiThau = CTkComboBox(master=app, 
+                       values=year_array,
+                    #    fg_color="#0093E9",
+                    #    border_color="#FBAB7E", 
+                    #    dropdown_fg_color="#0093E9",
                        command=change_handler)
 
 # Set the default value to the current year
-combobox.set(current_year)
-
+COMBOBOX_NamGoiThau.set(current_year)
 # Place the combobox in the window
-combobox.place(x=50, y=100)
+COMBOBOX_NamGoiThau.place(x=50, y=100)
 
 # ======================================================================================
-# LABEL_01_SoThongBaoMoiThau
+# LABEL_SoThongBaoMoiThau
 # ======================================================================================
-LABEL_01_SoThongBaoMoiThau = CTkLabel(master=app, text="Số thông báo mời thầu", font=("Arial", 14), text_color="#FFCC70")
-LABEL_01_SoThongBaoMoiThau.place(x=50, y=170)
+LABEL_SoThongBaoMoiThau = CTkLabel(master=app, 
+                                   text="Số thông báo mời thầu"
+                                    ,font=("",18)
+                                    # ,text_color="#FFCC70"
+                                   )
+LABEL_SoThongBaoMoiThau.place(x=50, y=170)
 
 # ======================================================================================
-# LABEL_02_TenThuMucSeKhoiTao
+# LABEL_TenThuMucSeKhoiTao
 # ======================================================================================
-LABEL_02_TenThuMucSeKhoiTao = CTkLabel(master=app, text="24_GOI_THAU_0001_15234685", font=("Arial", 14), text_color="#FFCC70")
-LABEL_02_TenThuMucSeKhoiTao.place(x=50, y=660)
+LABEL_TenThuMucSeKhoiTao = CTkLabel(master=app, 
+                                    text="24_GOI_THAU_0001_15234685"
+                                    ,font=("",18)
+                                    # ,text_color="#FFCC70"
+                                    )
+LABEL_TenThuMucSeKhoiTao.place(x=50, y=660)
+
+# ======================================================================================
+# LABEL_NamGoiThau
+# ======================================================================================
+LABEL_NamGoiThau = CTkLabel(master=app
+                    ,text="Năm gói thầu"
+                    ,font=("",18)
+                    # ,text_color="#FFCC70"
+                    )
+LABEL_NamGoiThau.place(x=50, y=70)
+
+# ======================================================================================
+# LABEL_DanhSachGoiThau
+# ======================================================================================
+LABEL_DanhSachGoiThau = CTkLabel(master=app
+                                    ,text="Danh sách gói thầu"
+                                    ,font=("",18)
+                                    # ,text_color="#FFCC70"
+                                    )
+LABEL_DanhSachGoiThau.place(x=50, y=270)
+
+# ======================================================================================
+# LABEL_IDNhanVien
+# ======================================================================================
+LABEL_IDNhanVien = CTkLabel(master=app
+                            ,text="ID: TBD001"
+                            ,font=("",13)
+                            # ,text_color="#FFCC70"
+                            )
+LABEL_IDNhanVien.place(x=50, y=10)
+
+# ======================================================================================
+# LABEL_TenNhanVien
+# ======================================================================================
+LABEL_TenNhanVien = CTkLabel(master=app
+                            ,text="Họ tên: Nguyễn Văn B"
+                            ,font=("",13)
+                            # ,text_color="#FFCC70"
+                             )
+LABEL_TenNhanVien.place(x=150, y=10)
+
+# ======================================================================================
+# LABEL_TenBoPhan
+# ======================================================================================
+LABEL_TenBoPhan = CTkLabel(master=app
+                           ,text="Bộ phận: Kinh doanh"
+                           ,font=("",13)
+                        #    ,text_color="#FFCC70"
+                           )
+LABEL_TenBoPhan.place(x=350, y=10)
+
+# ======================================================================================
+# LABEL_CapBac
+# ======================================================================================
+LABEL_CapBac = CTkLabel(master=app
+                        ,text="Cấp bậc: Nhân viên"
+                        ,font=("",13)
+                        # ,text_color="#FFCC70"
+                        )
+LABEL_CapBac.place(x=550, y=10)
+
+# ======================================================================================
+# LABEL_TenCongTy
+# ======================================================================================
+LABEL_TenCongTy = CTkLabel(master=app
+                           ,text="Cty: Thiết bị điện"
+                           ,font=("",13)
+                        #    ,text_color="#FFCC70"
+                           )
+LABEL_TenCongTy.place(x=750, y=10)
 
 # ======================================================================================
 # ENTRY_01_SoThongBaoMoiThau
 # ======================================================================================
-ENTRY_01_SoThongBaoMoiThau = CTkEntry(master=app, 
-                                        placeholder_text="Start typing...", 
-                                        width=300,
-                                        text_color="#FFCC70")
+def update_label(*args):
+    LABEL_TenThuMucSeKhoiTao.configure(text=ENTRY_01_SoThongBaoMoiThau.get())
+    
+ENTRY_01_SoThongBaoMoiThau = CTkEntry(master=app
+                                        ,placeholder_text="Start typing..."
+                                        ,width=300
+                                        # ,text_color="#FFCC70"
+                                        )
 ENTRY_01_SoThongBaoMoiThau.place(x=50, y=200)
+ENTRY_01_SoThongBaoMoiThau.bind("<KeyRelease>", update_label)
 
+# ======================================================================================
+# LISTBOX
+# ======================================================================================
+# Function to list all files and folders in the directory
+def list_directory_contents(directory):
+    try:
+        contents = os.listdir(directory)
+        return contents
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+        
+# Global variable to keep track of the currently highlighted label
+highlighted_label_now = None
+# Function to handle click event on labels
+def on_label_click(event):
+    global highlighted_label_now
+    label = event.widget
+    print(f"Clicked on: {label.cget('text')}")
+    # Reset the background color of the previously highlighted label
+    for child in label.master.winfo_children():
+        child.configure(bg="#333333")
+    if highlighted_label_now:
+        highlighted_label_now.configure(bg="#333333")
+        # Highlight the clicked label
+        label.configure(bg="#FFCC70")
+        # Update the highlighted label
+        highlighted_label_now = label
 
+# Function to populate the Frame with directory contents
+def populate_frame(frame, contents):
+    for item in contents:
+        label = CTkLabel(master=frame
+                         ,text=item
+                        #  ,font=("Arial", 12)
+                        #  ,text_color="#FFFFFF"
+                         )
+        label.pack(anchor="w")
+        label.bind("<Button-1>", on_label_click)
+
+def reset_scrollable_frame():
+    # Clear existing contents
+    for widget in scrollable_frame.winfo_children():
+        widget.destroy()
+
+def set_scrollable_frame():
+    # Get the current directory (or specify a different directory)
+    # current_directory = os.getcwd()
+    current_directory = r"\\172.16.0.191\2.0 ksnb\TUAN_AN_GROUP\BAN_KINH_DOANH\QUAN_LY_THAU"
+
+    # List the contents of the directory
+    directory_contents = list_directory_contents(current_directory)
+
+    # Sort the contents in descending order
+    directory_contents.sort(reverse=True)
+
+    # Populate the Frame with the directory contents
+    populate_frame(scrollable_frame, directory_contents)
+
+    # Scroll to the end by default
+    scrollable_frame.update_idletasks()
+    # scrollable_frame._parent_canvas.yview_moveto(1.0)       # Scroll to the end
+    scrollable_frame._parent_canvas.yview_moveto(0.0)
+
+scrollable_frame = CTkScrollableFrame(master=app
+                                        ,width=800
+                                        ,height=200
+                                        # ,fg_color="#333333"
+                                        )
+scrollable_frame.place(x=50, y=300)
+
+set_scrollable_frame()
+# ======================================================================================
+# MAINLOOP
+# ======================================================================================
 app.mainloop()
