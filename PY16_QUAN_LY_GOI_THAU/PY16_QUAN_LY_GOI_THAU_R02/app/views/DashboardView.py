9d78919c3bfc87b4cc5c33aa43af277f28e9113b
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2024-12-05 09:47:50 +0700

    commit

--- PY16_QUAN_LY_GOI_THAU/PY16_QUAN_LY_GOI_THAU_R02/app/views/DashboardView.py
+++
@@ -1,6 +0,0 @@
-class Dashboard:
-    def __init__(self):
-        print("Dashboard initialized")
-
-    def run(self):
-        print("Running the dashboard")
--- PY16_QUAN_LY_GOI_THAU/PY16_QUAN_LY_GOI_THAU_R06/app/views/components/__init__.py
+++
@@ -1,9 +0,0 @@
-# Project/views/components/__init__.py
-
-from .menu import create_top_menu
-from .main_content import create_main_content
-from .footer import create_footer
-from .header import create_header
-from .left_menu import create_left_menu
-from .right_banner import create_right_banner
-from .notification import create_notification
