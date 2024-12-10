22996270b17bef4575b181f9d3b87fc9bdcc9fda
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2024-12-10 17:01:14 +0700

    commit

--- PY19_ERP_TAG_THUONG_MAI/app/views/dashboard/DashboardView.py
+++ PY19_ERP_TAG_THUONG_MAI/app/views/dashboard/DashboardView.py
@@ -10,16 +10,16 @@ def f_render_dashboard():
     
     # Thiết lập kích thước cửa sổ
     set_window_size(root)
-
+    
     # # Kiểm tra đường dẫn của hàm set_window_size
     # f_find_my_function_path(set_window_size)
     
     # Gọi các thành phần tái sử dụng
-    create_header(root)
     cls_Menu(root, root)
+    
+    create_header(root)
     create_left_menu(root)
     create_right_banner(root)
-    
     create_footer(root)
     
     # Start the Tkinter main loop
