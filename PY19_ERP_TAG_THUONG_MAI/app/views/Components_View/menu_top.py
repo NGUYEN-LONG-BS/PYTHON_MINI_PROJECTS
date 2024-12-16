a3c9f1b1e57db1fd419ec8f876b3161ab95fd330
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2024-12-16 16:29:52 +0700

    commit

--- PY19_ERP_TAG_THUONG_MAI/app/views/Components_View/menu_top.py
+++ PY19_ERP_TAG_THUONG_MAI/app/views/Components_View/menu_top.py
@@ -45,36 +45,36 @@ class cls_menu_top:
             KinhDoanh_menu_Level_0 = tk.Menu(top_menu, tearoff=0)
             top_menu.add_cascade(label="Kinh doanh", menu=KinhDoanh_menu_Level_0)
             
-            # Create the third level (Submenu under Edit)
-            KinhDoanh_menu_Level_1 = tk.Menu(KinhDoanh_menu_Level_0, tearoff=0)
-            KinhDoanh_menu_Level_0.add_cascade(label="Quản lý yêu cầu đặt hàng", menu=KinhDoanh_menu_Level_1)
+            # Sub-menu level 1
+            KinhDoanh_menu_Level_1_KD01 = tk.Menu(KinhDoanh_menu_Level_0, tearoff=0)
+            KinhDoanh_menu_Level_0.add_cascade(label="Quản lý gói thầu", menu=KinhDoanh_menu_Level_1_KD01)
+            KinhDoanh_menu_Level_0.add_separator()
+            
+            KinhDoanh_menu_Level_1_KD02 = tk.Menu(KinhDoanh_menu_Level_0, tearoff=0)
+            KinhDoanh_menu_Level_0.add_cascade(label="Quản lý yêu cầu đặt hàng", menu=KinhDoanh_menu_Level_1_KD02)
+            KinhDoanh_menu_Level_0.add_separator()
+            
+            KinhDoanh_menu_Level_1_KD02.add_command(label="KD0201 |Tạo mới YCĐH", command=self.f_QLYCDH_TALA_click)
+            KinhDoanh_menu_Level_1_KD02.add_command(label="KD0202 |Nhật ký YCĐH", command=self.f_QLYCDH_TM_click)
 
-            # Add suboptions to the submenu
-            KinhDoanh_menu_Level_1.add_command(label="KD0201 |Tạo mới YCĐH", command=self.f_QLYCDH_TALA_click)
-            KinhDoanh_menu_Level_1.add_command(label="KD0202 |Nhật ký YCĐH", command=self.f_QLYCDH_TM_click)
-
-            # Create the fourth level (Subsubmenu under Submenu)
-            KinhDoanh_menu_Level_2 = tk.Menu(KinhDoanh_menu_Level_1, tearoff=0)
-            KinhDoanh_menu_Level_1.add_cascade(label="Subsubmenu", menu=KinhDoanh_menu_Level_2)
-
-            # Add subsuboptions to the subsubmenu
+            # Sub-menu level 2
+            KinhDoanh_menu_Level_2 = tk.Menu(KinhDoanh_menu_Level_1_KD02, tearoff=0)
+            KinhDoanh_menu_Level_1_KD02.add_cascade(label="Subsubmenu", menu=KinhDoanh_menu_Level_2)
             KinhDoanh_menu_Level_2.add_command(label="Subsuboption 1", command=self.f_do_nothing_click)
             KinhDoanh_menu_Level_2.add_command(label="Subsuboption 2", command=self.f_do_nothing_click)
             
             KinhDoanh_menu_Level_0.add_separator()
             KinhDoanh_menu_Level_0.add_command(label="KD0201 |Phiếu Yêu cầu đặt hàng", command=self.f_QLYCDH_TALA_click)
             KinhDoanh_menu_Level_0.add_command(label="KD0202 |Nhật ký yêu cầu đặt hàng", command=self.f_QLYCDH_TM_click)
+            
             KinhDoanh_menu_Level_0.add_separator()
             KinhDoanh_menu_Level_0.add_command(label="KD0101 |Quản lý gói thầu", command=self.f_KD0101_QuanLyGoiThau_click)
             KinhDoanh_menu_Level_0.add_command(label="KD0102 |Tạo mới gói thầu", command=self.f_KD0101_QuanLyGoiThau_click)
             KinhDoanh_menu_Level_0.add_command(label="Tạo mới gói thầu", command=self.f_QLGT_TaoMoi_click)
             KinhDoanh_menu_Level_0.add_command(label="Các gói thầu đã lập", command=self.f_QLGT_GoiThauDaLap_click)
+            
             KinhDoanh_menu_Level_0.add_separator()
             KinhDoanh_menu_Level_0.add_command(label="Quản lý khách hàng", command=self.f_do_nothing_click)
-            # self.f_create_KinhDoanh_menu()
-            
-            
-            
             
         # menu của Vật Tư
         khong_co_quyen_vat_tu = ["kd1", "tc1", "kt1"]
@@ -83,9 +83,11 @@ class cls_menu_top:
             # print("không khỏi tạo menu vật tư")
         else:
             VatTu_menu_Level_0 = tk.Menu(top_menu, tearoff=0)
+            top_menu.add_cascade(label="Vật Tư", menu=VatTu_menu_Level_0)
+            
             VatTu_menu_Level_0.add_command(label="DS Yêu cầu đặt hàng", command=self.f_do_nothing_click)
             VatTu_menu_Level_0.add_command(label="QL Nhà Cung cấp", command=self.f_do_nothing_click)
-            top_menu.add_cascade(label="Vật Tư", menu=VatTu_menu_Level_0)
+            
     
         # menu của Kỹ thuật
         khong_co_quyen_ky_thuat = ["kd1", "tc1", "vt1"]
@@ -94,9 +96,11 @@ class cls_menu_top:
             # print("không khỏi tạo menu kỹ thuật")
         else:
             KyThuat_menu_Level_0 = tk.Menu(top_menu, tearoff=0)
+            top_menu.add_cascade(label="Kỹ thuật", menu=KyThuat_menu_Level_0)
+            
             KyThuat_menu_Level_0.add_command(label="Yêu cầu KT 01", command=self.f_do_nothing_click)
             KyThuat_menu_Level_0.add_command(label="Yêu cầu KT 02", command=self.f_do_nothing_click)
-            top_menu.add_cascade(label="Kỹ thuật", menu=KyThuat_menu_Level_0)
+            
             
         # menu của Tài chính
         khong_co_quyen_tai_chinh = ["kd1", "kt1", "vt1"]
@@ -105,23 +109,39 @@ class cls_menu_top:
             # print("không khỏi tạo menu kỹ thuật")
         else:
             TaiChinh_menu_Level_0 = tk.Menu(top_menu, tearoff=0)
-            TaiChinh_menu_Level_0.add_command(label="Quỹ tiền mặt", command=self.f_do_nothing_click)
-            TaiChinh_menu_Level_0.add_command(label="Quỹ tiền gửi", command=self.f_do_nothing_click)
             top_menu.add_cascade(label="Tài chính", menu=TaiChinh_menu_Level_0)
+            
+            # Sub-menu level 1
+            TaiChinh_menu_Level_1 = tk.Menu(KinhDoanh_menu_Level_0, tearoff=0)
+            TaiChinh_menu_Level_0.add_cascade(label="Quản lý thu chi", menu=TaiChinh_menu_Level_1)
+            TaiChinh_menu_Level_1.add_command(label="TC0101 |Quỹ tiền mặt", command=self.f_do_nothing_click)
+            TaiChinh_menu_Level_1.add_command(label="TC0102 |Quỹ tiền gửi", command=self.f_do_nothing_click)
+            TaiChinh_menu_Level_1.add_command(label="TC0103 |Thu chi tiền mặt", command=self.f_do_nothing_click)
+            TaiChinh_menu_Level_1.add_command(label="TC0104 |Thu chi tiền gửi", command=self.f_do_nothing_click)
+            
+            # Sub-menu level 1
+            TaiChinh_menu_Level_0.add_cascade(label="Quản lý thu chi", menu=TaiChinh_menu_Level_1)
+            TaiChinh_menu_Level_1.add_command(label="TC0101 |Quỹ tiền mặt", command=self.f_do_nothing_click)
+            TaiChinh_menu_Level_1.add_command(label="TC0102 |Quỹ tiền gửi", command=self.f_do_nothing_click)
+            TaiChinh_menu_Level_1.add_command(label="TC0103 |Thu chi tiền mặt", command=self.f_do_nothing_click)
+            TaiChinh_menu_Level_1.add_command(label="TC0104 |Thu chi tiền gửi", command=self.f_do_nothing_click)
+            
         
         # Create a "Help" menu
         HELP_menu_Level_0 = tk.Menu(top_menu, tearoff=0)
+        top_menu.add_cascade(label="Help", menu=HELP_menu_Level_0)
+        
         HELP_menu_Level_0.add_command(label="About", command=self.f_Help_About_click)
         HELP_menu_Level_0.add_command(label="User Info", command=self.f_Help_UserInfo_click)
         HELP_menu_Level_0.add_separator()
         HELP_menu_Level_0.add_command(label="Sign out", command=self.f_Help_Signout_click)
         HELP_menu_Level_0.add_command(label="Exit", command=self.f_Help_Exit_click)
-        top_menu.add_cascade(label="Help", menu=HELP_menu_Level_0)
+        
         
         # Set font size to 15 for all menus
         f_set_menu_font(HOME_menu_Level_0)
         f_set_menu_font(KinhDoanh_menu_Level_0)
-        f_set_menu_font(KinhDoanh_menu_Level_1)
+        f_set_menu_font(KinhDoanh_menu_Level_1_KD02)
         f_set_menu_font(KinhDoanh_menu_Level_2)
         f_set_menu_font(VatTu_menu_Level_0)
         f_set_menu_font(KyThuat_menu_Level_0)
