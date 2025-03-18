bedd80cae44c02eb874542a711ff25fcecb13253
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-03-18 08:16:57 +0700

    commit

---
+++ PY_FLASK_PROJECT_beginner/video_04_02_su_dung_boostrap/app.py
@@ -0,0 +1,22 @@
+# https://www.youtube.com/watch?v=PoKKO48A_pg&list=PLFyAEmibWSQCc60nNQByzmtbMjk3fGyIK&index=5
+# LẬP TRÌNH WEB FLASK-PYTHON #4: KẾ THỪA TEMPLATE, SỬ DỤNG BOOTSTRAP
+
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_home_page():
+    return render_template("home.html"
+                           ,var_name="Kế thừa templates"
+                           )
+    
+@app.route('/index')
+def hello_world():
+    return render_template("index.html"
+                           ,var_name="biến đây"
+                           ,cars=["Vin", "BMW", "Audi", "Toyota", "Honda", "Suzuki", "Mazda"]
+                           )
+    
+if __name__ == '__main__':
+    app.run(debug=True)
