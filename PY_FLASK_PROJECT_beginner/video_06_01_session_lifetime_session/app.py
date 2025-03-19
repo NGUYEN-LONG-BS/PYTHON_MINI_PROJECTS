5f7668962921345df80707100af25348a780dc95
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-03-19 11:13:20 +0700

    commit

--- PY_FLASK_PROJECT_beginner/video_06_01_session_lifetime_session/app.py
+++ PY_FLASK_PROJECT_beginner/video_06_01_session_lifetime_session/app.py
@@ -2,11 +2,17 @@
 # LẬP TRÌNH WEB FLASK-PYTHON #6: SESSION LÀ GÌ, LIFETIME SESSION
 
 from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
-app.config["SECERET_KEY"] = "thanhdz"
+app.config["SECRET_KEY"] = "thanhdz"
+app.permanent_session_lifetime = timedelta(minutes=1)
 
 @app.route('/')
+def render_start():
+    return redirect(url_for('login'))
+
+@app.route('/home')
 def render_home_page():
     return render_template("home.html"
                            ,var_name="Kế thừa templates"
@@ -17,9 +23,10 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        session.permanent = True  # Làm cho session luôn có lifetime
         if username == 'admin' and password == '123456':
-            session['username'] = username
-            return redirect(url_for('hello_user', name='admin'))
+            session['username'] = username  # Lưu trữ tên người dùng vào session
+            return redirect(url_for("hello_user", name=username))
     return render_template("login.html")
     
 @app.route('/index')
@@ -31,11 +38,17 @@ def hello_world():
 
 @app.route('/user')
 def hello_user():
+    print("chuyển sang hello_user")
     if 'username' in session:
         name = session['username']
         return f"<H1>Hello {name}</H1>"
     else:
         return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
     
 if __name__ == '__main__':
     app.run(debug=True)
