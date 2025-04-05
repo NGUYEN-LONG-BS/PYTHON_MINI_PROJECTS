73125b6539568872994e903bc2a2d8097d02edc1
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-04-05 14:34:11 +0700

    add more controller

--- PY_ERP_PROJECT/PY21_ERP_TAG_THUONG_MAI/MY_PROJECT/app/views/VT00_QuanLyHangHoa_View/__init__.py
+++ PY_ERP_PROJECT/PY21_ERP_TAG_THUONG_MAI/MY_PROJECT/app/views/VT00_QuanLyHangHoa_View/__init__.py
@@ -3,3 +3,6 @@ from .controller_handle_events import *
 from .controller_tab_01_PNK import *
 from .controller_tab_02_PXK import *
 from .controller_tab_03_new_id import *
+from .controller_tab_04 import *
+from .controller_tab_05 import *
+from .controller_tab_06 import *
